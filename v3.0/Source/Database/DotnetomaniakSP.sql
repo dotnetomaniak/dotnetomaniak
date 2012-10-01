dc0d501adcc570fd6f88e456e9799153cda86664
Pawel Lukasik <lukasik.pawel+github@gmail.com>
2012-10-02 00:23:27 +0200

    Fixing dotnetomaniak badge

--- v3.0/Source/Database/DotnetomaniakSP.sql
+++ v3.0/Source/Database/DotnetomaniakSP.sql
@@ -1,4 +1,6 @@
--- ================================================
+USE [pawlos_dotnetomaniak]
+GO
+/****** Object:  StoredProcedure [pawlos_dotnetomaniak].[Dotnetomaniak]    Script Date: 10/01/2012 21:26:53 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -8,7 +10,7 @@ GO
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-CREATE PROCEDURE Dotnetomaniak	
+ALTER PROCEDURE [pawlos_dotnetomaniak].[Dotnetomaniak]	
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -16,15 +18,18 @@ BEGIN
     SET NOCOUNT ON;
 	
     INSERT INTO UserAchievement (AchievementId,Displayed,UserId,DateAchieved)
-        select 'C5217D3A-1CA1-4C49-9E4C-990C63CD7176',0,UserId, TimeStamp from UserScore where Actiontype = 1
-	and UserId NOT IN (SELECT UserId FROM UserAchievement WHERE AchievementId = 'C5217D3A-1CA1-4C49-9E4C-990C63CD7176')
-     UNION
-        SELECT 'C5217D3A-1CA1-4C49-9E4C-990C63CD7176',0,[Id] ,[CreatedAt]
+        SELECT a.Achievement, a.[Type], a.UserId, MIN(a.[TimeStamp]) FROM
+(
+	select 'C5217D3A-1CA1-4C49-9E4C-990C63CD7176' as Achievement,0 as [Type],UserId, TimeStamp from UserScore where Actiontype = 1
+		and UserId NOT IN (SELECT UserId FROM UserAchievement WHERE AchievementId = 'C5217D3A-1CA1-4C49-9E4C-990C63CD7176')
+    UNION
+    SELECT 'C5217D3A-1CA1-4C49-9E4C-990C63CD7176'as Achievement,0 as [Type],[Id] as UserId ,[CreatedAt] as TimeStamp
         FROM [User]
 	WHERE 
 		Password IS NULL AND
 		IsActive = 1 AND
 		Id NOT IN (SELECT UserId FROM UserAchievement WHERE AchievementId = 'C5217D3A-1CA1-4C49-9E4C-990C63CD7176')
+) a
+GROUP BY Achievement, [Type], [UserId]
     SELECT 1;
 END
-GO
