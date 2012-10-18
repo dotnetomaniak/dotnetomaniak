062fc2d5271420a8bc97b786e5c6014ced549cb8
Piotr Gankiewicz <spetz90@gmail.com>
2012-10-18 09:56:43 +0200

    Closes #8

--- v3.0/Source/Web/Extension/DateTimeExtension.cs
+++ v3.0/Source/Web/Extension/DateTimeExtension.cs
@@ -59,5 +59,46 @@ namespace Kigg.Web
 
             return (result.Length == 0) ? "chwilê" : result.ToString();
         }
+
+
+
+        public static DateTime GetLastWeekdayOfMonth(this DateTime date, DayOfWeek day)
+        {
+            DateTime lastDayOfMonth = new DateTime(date.Year, date.Month, 1).AddMonths(1).AddDays(-1);
+            int wantedDay = (int)day;
+            int lastDay = (int)lastDayOfMonth.DayOfWeek;
+            return lastDayOfMonth.AddDays(lastDay >= wantedDay ? wantedDay - lastDay : wantedDay - lastDay - 7);
+        }
+
+        public static string GetLocalTimeName(this DateTime date, bool? isSummerTime = null)
+        {
+            if (isSummerTime == null)
+                isSummerTime = IsSummerTime(date);
+
+            return isSummerTime.Value ? "CEST" : "CET";
+        }
+
+        public static int GetHoursDifferenceForLocalTime(this DateTime date, bool? isSummerTime = null)
+        {
+            if (isSummerTime == null)
+                isSummerTime = IsSummerTime(date);
+
+            return isSummerTime.Value ? 1 : -1;
+        }
+
+        public static bool IsSummerTime(this DateTime date)
+        {
+            var lastSundayInMarch = GetLastWeekdayOfMonth(new DateTime(date.Year, 3, 1), DayOfWeek.Sunday);
+            var lastSundayInOctober = GetLastWeekdayOfMonth(new DateTime(date.Year, 10, 1), DayOfWeek.Sunday);
+
+            if ((date > lastSundayInMarch || date == lastSundayInMarch && date.Hour >= 2))
+                if ((date < lastSundayInOctober || date == lastSundayInOctober && date.Hour <= 3))
+                    return true;
+
+
+            return false;
+        }
+
+
     }
 }
