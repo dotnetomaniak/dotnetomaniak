db19a9a7acb44c63db72ee6e094b3d8ee349d974
Pawel Kubacki <pawel.kubacki@octal.pl>
2013-04-30 11:02:57 +0200

    Dodanie MiniProfilera do projektu Dotnetomaniak

--- v3.0/Source/Web/Properties/AssemblyInfo.cs
+++ v3.0/Source/Web/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 //using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 [assembly: AssemblyTitle("Kigg.Web")]
 [assembly: AssemblyProduct("Kigg.Web")]
