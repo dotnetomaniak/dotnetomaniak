7b8e569cbf11f8ae9c9473add44d4fad95343156
Pawel Kubacki <pawel.kubacki@octal.pl>
2013-05-14 12:10:48 +0200

    Dodanie widoku

---
+++ v3.0/Source/Web/ViewData/RecommendationViewData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kigg.Web.ViewData
+{
+    public class RecommendationViewData : BaseViewData
+    {
+        public string UrlLink { get; set; }
+        public string UrlTitle { get; set; }
+        public string ImageName { get; set; }
+        public string ImageAlt { get; set; }
+    }
+}
