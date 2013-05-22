28e07053e4a73f7be8d536b792b6b73202177e06
Pawel Kubacki <pawel.kubacki@octal.pl>
2013-05-22 13:52:04 +0200

    #40 aplikacja do zmiany adresu URL postow

---
+++ v3.0/Extra/PostMapper/Hash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PostMaping
+{
+    public class Hash
+    {
+        public static string ComputeHash(string target)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] data = Encoding.Unicode.GetBytes(target);
+                byte[] hash = md5.ComputeHash(data);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
---
+++ v3.0/Extra/PostMapper/UrlAddres.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Web;
+
+namespace PostMaping
+{
+    public class UrlAddres
+    {
+        public string QuestionUrl(string oldUrl)
+        {
+            string whereToSearch = oldUrl + " site:www.piotrludwiczuk.net";
+            string newUrl = "https://www.google.pl/search?q=" + HttpUtility.UrlEncode(whereToSearch) + "&hl=pl";
+            return newUrl;
+        }
+
+        public string SearchForNewUrl(string siteInString)
+        {
+            int biginOfString = siteInString.IndexOf("main");
+            int endOfString = siteInString.IndexOf("/body>", biginOfString + 1);
+            string halfSiteInString = siteInString.Substring(biginOfString, endOfString - biginOfString);
+
+            biginOfString = halfSiteInString.IndexOf("http://www.piotrludwiczuk.net");
+            endOfString = halfSiteInString.IndexOf(".aspx", biginOfString + 1);
+            string encodedString = halfSiteInString.Substring(biginOfString, endOfString - biginOfString);
+            return Uri.UnescapeDataString(HttpUtility.UrlDecode(encodedString)) + ".aspx";       
+        }
+
+        public bool SprawdzStrone(string url)
+        {
+            try
+            {
+                //Creating the HttpWebRequest
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                //Setting the Request method HEAD, you can also use GET too.                
+                request.Method = "HEAD";
+                //Getting the Web Response.
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    //Returns TRUE if the Status code == 200
+                    return (response.StatusCode == HttpStatusCode.OK);
+                }
+            }
+            catch
+            {
+                //Any exception will returns false.
+                return false;
+            }
+        }        
+    }
+}
