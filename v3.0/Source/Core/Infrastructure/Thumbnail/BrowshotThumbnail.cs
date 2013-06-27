dce0c1a822bd9de7816f4bd2053c4213bd184d32
Pawel Kubacki <pawel.kubacki@octal.pl>
2013-06-27 07:10:24 +0200

    #58 dodano encodowanie dla browshot.

--- v3.0/Source/Core/Infrastructure/Thumbnail/BrowshotThumbnail.cs
+++ v3.0/Source/Core/Infrastructure/Thumbnail/BrowshotThumbnail.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using Kigg.Core.Infrastructure.Thumbnail;
 using System;
 
 namespace Kigg.Infrastructure
@@ -10,17 +11,20 @@ namespace Kigg.Infrastructure
         private readonly string _baseUrl;
         private readonly int _instanceId;
         private readonly IHttpForm _httpForm;
+        private readonly IEncoder _encoder;
 
-        public BrowshotThumbnail(string key, string baseUrl, int instanceId, IHttpForm httpForm)
+        public BrowshotThumbnail(string key, string baseUrl, int instanceId, IHttpForm httpForm, IEncoder encoder)
         {
             Check.Argument.IsNotEmpty(key, "key");
             Check.Argument.IsNotEmpty(baseUrl, "baseUrl");
             Check.Argument.IsNotNull(httpForm, "httpForm");
+            Check.Argument.IsNotNull(encoder, "encoder");
 
             _key = key; 
             _baseUrl = baseUrl;
             _instanceId = instanceId;
             _httpForm = httpForm;
+            _encoder = encoder;
         }
 
         public string For(string url, ThumbnailSize inSize)
@@ -29,7 +33,7 @@ namespace Kigg.Infrastructure
 
             string size = inSize == ThumbnailSize.Small ? "width=111&height=111" : "height=250";//width=250&
 
-            return "{0}?url={1}&instance_id={2}&{3}&key={4}".FormatWith(_baseUrl, url, _instanceId, size, _key);
+            return "{0}?url={1}&instance_id={2}&{3}&key={4}".FormatWith(_baseUrl,_encoder.EncodeUrl(url), _instanceId, size, _key);
         }
 
         public void Capture(string url)
