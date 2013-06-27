5cf930c41f58ffa7b9e2108ab8b20b90c88ba1ef
Pawel Kubacki <pawel.kubacki@octal.pl>
2013-06-27 07:10:24 +0200

    #58 dodano encodowanie dla browshot.

---
+++ v3.0/Source/Web/Helpers/Encoder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kigg.Core.Infrastructure.Thumbnail;
+
+namespace Kigg.Web.Helpers
+{
+    public class Encoder : IEncoder
+    {
+        private string _url;
+
+        public Encoder(string url)
+        {
+            Check.Argument.IsNotEmpty(url,"url");
+            _url = url;
+        }
+
+        public string EncodeUrl(string _url)
+        {
+            return HttpUtility.UrlEncode(_url);
+        }
+    }
+}
