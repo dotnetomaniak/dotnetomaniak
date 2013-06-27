8f89451a33f0b16432388dbc7b4d204e80be7166
Pawel Kubacki <pawel.kubacki@octal.pl>
2013-06-27 10:34:32 +0200

    #22 Pozycja GoogleBox na dole strony.

---
+++ v3.0/Source/Web/Assets/JavaScripts/SocialMedia.js
@@ -0,0 +1,12 @@
+﻿var SocialMedia =
+    {
+        init: function () {
+            var mainHeight = $('#main').height();
+            var subHeight = $('#sub').height();
+            var subTotalHeight = subHeight + 200;
+            if (mainHeight > subTotalHeight)
+                $('#sub').height(mainHeight);
+            else
+                $('#sub').height(subTotalHeight);
+        }
+};
