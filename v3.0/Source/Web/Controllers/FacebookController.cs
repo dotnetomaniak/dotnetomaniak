84e7a79ec9374aca2e7e34188142bc9df0a97a87
Daniel Anuszczyk <daniel.anuszczyk@octal.pl>
2014-05-28 13:43:56 +0200

    #72 deserialize ajax data to fbuser model

---
+++ v3.0/Source/Web/Controllers/FacebookController.cs
@@ -0,0 +1,21 @@
+﻿using Kigg.Web.ViewData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace Kigg.Web.Controllers
+{
+    public class FacebookController : BaseController
+    {
+        //[AcceptVerbs(HttpVerbs.Post), Compress]
+        public ActionResult GetFBData(string data)
+        {
+            var myself = new JavaScriptSerializer().Deserialize<FbUserDataView>(data);                
+            
+            return Json(new {status = "hura" });
+        }
+    }
+}
