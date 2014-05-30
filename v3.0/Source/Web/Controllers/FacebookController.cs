770941c8a4c46b5bf38c7d3402c09958ec1c26e8
Daniel Anuszczyk <daniel.anuszczyk@octal.pl>
2014-05-30 09:40:18 +0200

    #72 FindUserByFbId is working

--- v3.0/Source/Web/Controllers/FacebookController.cs
+++ v3.0/Source/Web/Controllers/FacebookController.cs
@@ -6,15 +6,27 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
 
+using Kigg.Repository;
+
 namespace Kigg.Web.Controllers
 {
     public class FacebookController : BaseController
     {
+        private readonly IUserRepository _userRepository;
+
+        public FacebookController(IUserRepository userRepository)
+        {
+            Check.Argument.IsNotNull(userRepository, "userRepository");
+
+            _userRepository=userRepository;
+        }
+
         //[AcceptVerbs(HttpVerbs.Post), Compress]
         public ActionResult GetFBData(string data)
         {
             var myself = new JavaScriptSerializer().Deserialize<FbUserDataView>(data);                
             
+            var user = _userRepository.FindByFbId(myself.Id);
             return Json(new {status = "hura" });
         }
     }
