ff235abc8ad9e6357a152e4f7edd93acfe5890a9
Daniel Anuszczyk <daniel.anuszczyk@octal.pl>
2014-07-08 10:54:21 +0200

    Add missing files

---
+++ v3.0/Source/Web/ViewData/FbEmailViewData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kigg.Web
+{
+    public class FbEmailViewData : BaseViewData
+    {
+        public string Email { get; set; }
+    }
+}
