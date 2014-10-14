860dc82a60f960ccacddb045df3976323e81bc35
Pawel Kubacki <pawel.kubacki@octal.pl>
2014-10-14 08:35:24 +0200

    Added model for recomendation controller action.

---
+++ v3.0/Source/Web/ViewData/AdvertiseViewData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Kigg.Web.ViewData
+{
+    public class AdvertiseViewData
+    {
+        public string id { get; set; }
+        public string recommendationLink { get; set; }
+        public string recommendationTitle { get; set; }
+        public string imageLink { get; set; }
+        public string imageTitle { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public string email { get; set; }
+        public int position { get; set; }
+        public bool notificationIsSent { get; set; }
+        public string isBanner { get; set; }
+
+        public AdvertiseViewData()
+        {
+            isBanner = "";
+            notificationIsSent = false;
+            position = 999;
+        }
+    }
+}
