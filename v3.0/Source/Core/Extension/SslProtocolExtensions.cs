ccf4329b207b580804fc4c4b0d0c80dad3a865be
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2017-11-19 14:39:45 +0100

    Added SSL protocols supported

---
+++ v3.0/Source/Core/Extension/SslProtocolExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Authentication;
+using System.Text;
+
+namespace Kigg.Core.Extension
+{
+    public static class SslProtocolExtensions
+    {
+        public const SecurityProtocolType Tls12 = (SecurityProtocolType)0x00000C00;
+        public const SecurityProtocolType Tls11 = (SecurityProtocolType)0x00000300;
+    }
+}
