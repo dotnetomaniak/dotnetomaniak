78f02a29741d1a70e1519003add909b6efcdada4
Piotr Stapp <piotr.stapp@finai.com>
2018-06-16 11:54:24 +0200

    Cache story view count

--- v3.0/Source/LinqToSql/Repository/StoryViewRepository.cs
+++ v3.0/Source/LinqToSql/Repository/StoryViewRepository.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using Kigg.Infrastructure;
+
 namespace Kigg.LinqToSql.Repository
 {
     using System;
@@ -10,18 +13,36 @@ namespace Kigg.LinqToSql.Repository
     
     public class StoryViewRepository : BaseRepository<IStoryView, StoryView>, IStoryViewRepository
     {
-        public StoryViewRepository(IDatabase database) : base(database)
+        private static object _lockObj = new Object();
+        private readonly ICache _cache;
+
+        public StoryViewRepository(IDatabase database, ICache cache) : base(database)
         {
+            _cache = cache;
         }
 
-        public StoryViewRepository(IDatabaseFactory factory) : base(factory)
+        public StoryViewRepository(IDatabaseFactory factory, ICache cache) : base(factory)
         {
+            _cache = cache;
         }
 
         public virtual int CountByStory(Guid storyId)
         {
             Check.Argument.IsNotEmpty(storyId, "storyId");
-            return -1;
+            var views = Cache.Get<Dictionary<Guid, int>>("storyViewCount");
+            if (views == null)
+            {
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                views = Database.StoryViewDataSource.GroupBy(x => x.StoryId)
+                    .Select(x => new {StoryId = x.Key, Count = x.Count()})
+                    .ToDictionary(x => x.StoryId, x => x.Count);
+                
+                Cache.Set("storyViewCount", views, DateTime.Now.AddMinutes(15));
+                stopwatch.Stop();
+                Log.Info("Created cache storyViewCount in " + stopwatch.ElapsedMilliseconds);
+            }
+
+            return views.ContainsKey(storyId) ? views[storyId] : 0;
             //return Database.StoryViewDataSource.Count(v => v.StoryId == storyId);
         }
 
