932a33e63d2a0feed23fb07db1a2899b6d6b42bb
szymczakk <rakaz666@gmail.com>
2019-06-10 22:55:26 +0200

    add UnitOfWork

---
+++ v3.0/Source/Kigg.Infrastructure.EF/Repository/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Kigg.Infrastructure.EF.Repository
+{
+    public class UnitOfWork: DisposableResource, IUnitOfWork
+    {
+        private readonly DotnetomaniakContext _context;
+
+        public UnitOfWork(DotnetomaniakContext context)
+        {
+            _context = context;
+        }
+
+        private bool _isDisposed;
+
+        public virtual void Commit()
+        {
+            if (_isDisposed)
+            {
+                throw new ObjectDisposedException(GetType().Name);
+            }
+
+            _context.SaveChanges();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                _isDisposed = true;
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
