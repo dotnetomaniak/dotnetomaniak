eaa7ff29fff00464f5d2950fc45506ced4331b50
bpelikan <bartoszpelikan@gmail.com>
2019-06-21 20:45:24 +0200

    CreateSlot script

---
+++ v3.0/Source/Web/DeploymentScripts/CreateSlot.ps1
@@ -0,0 +1,17 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+    [String]
+    $ResourceGroupName,
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+    [String]
+    $ServicePlanName,
+	[Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+    [String]
+    $WebAppServiceName,
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+    [String] 
+    $SlotName
+)
+
+New-AzureRmWebAppSlot -ResourceGroupName $ResourceGroupName -Name $WebAppServiceName -AppServicePlan $ServicePlanName -Slot $SlotName > $null
